b77e207f47b4ba4a6b83e5ca37a6c79fd47828aa
rabiasyavuz <rrabiayavuz04@gmail.com>
2026-03-02 01:46:54 +0300

    Update tip.py

--- tip.py
+++ tip.py
@@ -1,1 +1,18 @@
+#5 Bahşiş Hesaplama 
+  def dollars_to_float(d):
+      return float(d.replace("$", ""))
+ 
+  def percent_to_float(p):
+      return float(p.replace("%", "")) / 100
+ 
+  meal = input("Yemek ne kadardı? ")
+  tip = input("Yüzde kaç bahşiş? ")
+ 
+  meal = dollars_to_float(meal)
+  tip = percent_to_float(tip)
+ 
+  tip_amount = meal * tip
+ 
+  print(f"Bahşiş: ${tip_amount:.2f}")
+
 
