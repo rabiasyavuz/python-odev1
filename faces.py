28484c8f34cb19deafe519ec989884b2946bdd97
rabiasyavuz <rrabiayavuz04@gmail.com>
2026-03-02 01:43:40 +0300

    Update faces.py

--- faces.py
+++ faces.py
@@ -1,1 +1,12 @@
+#3 İfade Dönüştürme
+ def convert(text):
+      text = text.replace(":)", "🙂")
+      text = text.replace(":(", "🙁")
+      return text
+ 
+ def main():
+      text = input("Metin girin: ")
+      print(convert(text))
+
+  main()
 
